895f9a2a8b30f0e7e1f9d4c87b367889669f3ce3
Amogo Solomon <amogoagaba@gmail.com>
2024-01-14 05:27:36 +0100

    updating slick carousal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
