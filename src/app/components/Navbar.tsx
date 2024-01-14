e75c8cddf592b417500bd29293b0b8f0b8bcd7dd
Amogo Solomon <amogoagaba@gmail.com>
2024-01-14 05:44:35 +0100

    upgating navlink

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,40 +1,55 @@
-import Link from "next/link"
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return ( 
-  	<div className='hidden lg:block'>
-    	<div className='container'>
-			<div className='flex w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
-				<Link className='navbar__link relative' href="#">
-					HOME
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					CATEGORIES
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					MEN'S
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					WOMEN'S
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					JEWELRY
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					PERFUME
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					BLOG
-				</Link>
-				<Link className='navbar__link relative' href="#">
-					HOT OFFERS
-				</Link>
-			</div>
-		</div>
+    <div className='hidden lg:block'>
+      <div className='container'>
+        <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
+          <Link href="/">
+            <a className='navbar__link relative'>
+              HOME
+            </a>
+          </Link>
+          <Link href="/categories">
+            <a className='navbar__link relative'>
+              CATEGORIES
+            </a>
+          </Link>
+          <Link href="/mens">
+            <a className='navbar__link relative'>
+              MEN'S
+            </a>
+          </Link>
+          <Link href="/womens">
+            <a className='navbar__link relative'>
+              WOMEN'S
+            </a>
+          </Link>
+          <Link href="/jewelry">
+            <a className='navbar__link relative'>
+              JEWELRY
+            </a>
+          </Link>
+          <Link href="/perfume">
+            <a className='navbar__link relative'>
+              PERFUME
+            </a>
+          </Link>
+          <Link href="/blog">
+            <a className='navbar__link relative'>
+              BLOG
+            </a>
+          </Link>
+          <Link href="/hot-offers">
+            <a className='navbar__link relative'>
+              HOT OFFERS
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
